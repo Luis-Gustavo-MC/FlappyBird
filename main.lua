8a5efedddc0e7aad7c6326e74846d056d04221bd
Luis-Gustavo-MC <lgmc120@gmail.com>
2025-06-03 20:48:23 -0300

    update, paralax, loop background

--- main.lua
+++ main.lua
@@ -10,9 +10,20 @@ ALTURA_VIRTUAL = 288
 local background = love.graphics.newImage('background.png') 
 local ground = love.graphics.newImage('ground.png')
 
+local backgroundScroll = 0
+local groundScroll = 0
+
+--Paralax effect
+local GROUND_SPEED = 60
+local BACKGROUND_SPEED = 30
+
+--LOOP 
+local BACKGROUND_LOOP = 413
+local GROUND_LOOP = 413
+
 function love.load()
-    --love.graphics.setDefaultFilter('near' , 'nearest') --Retro Filter
-    love.graphics.setDefaultFilter('nearest' , 'nearest')
+    love.window.setTitle("Flappy Bird") -- Name Window
+    love.graphics.setDefaultFilter('nearest' , 'nearest') --Retro Filtro
     push:setupScreen(LARGURA_VIRTUAL,ALTURA_VIRTUAL,LARGURA_JANELA,ALTURA_JANELA)
 
     vsync = true
@@ -31,11 +42,16 @@ function love.keypressed(tecla)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SPEED * dt) % BACKGROUND_LOOP
+    groundScroll = (groundScroll + GROUND_SPEED * dt) % GROUND_LOOP
+end
+
 function love.draw()
     push:start()
 
-    love.graphics.draw(background,0,0)
-    love.graphics.draw(ground, 0 , ALTURA_VIRTUAL - 16)
+    love.graphics.draw(background,-backgroundScroll,0)
+    love.graphics.draw(ground, -groundScroll, ALTURA_VIRTUAL - 16)
     
     push:finish()
 end
